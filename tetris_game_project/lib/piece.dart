b2459f2785cef8f53181ac91338ee5edd0702877
Kunal Thakurani <101013235+kunalthakurani19@users.noreply.github.com>
2023-07-26 18:21:57 +0530

    resolve the error

--- tetris_game_project/lib/piece.dart
+++ tetris_game_project/lib/piece.dart
@@ -1,74 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:tetris_game_project/values.dart';
-
+import 'package:flutter/services.dart';
 import 'board.dart';
 
+import 'values.dart';
+
 class Piece {
-  // type of tetris pices
   Tetromino type;
 
   Piece({required this.type});
 
-  // the piece is just a list of intergers
   List<int> position = [];
 
-  // color of tetris piece
   Color get color {
-    return tetrominoColors[type] ?? Colors.white;
+    return tetrominoColors[type] ?? const Color(0xFFFFFFFF);
   }
 
-  //generate the intergers
   void initializePiece() {
     switch (type) {
       case Tetromino.L:
-        position = [
-          -26,
-          -16,
-          -6,
-          -5,
-        ];
+        position = [-26, -16, -6, -5];
+        break;
       case Tetromino.J:
-        position = [
-          -25,
-          -15,
-          -5,
-          -6,
-        ];
+        position = [-25, -15, -5, -6];
+        break;
       case Tetromino.I:
-        position = [
-          -4,
-          -5,
-          -6,
-          -7,
-        ];
+        position = [-4, -5, -6, -7];
+        break;
       case Tetromino.O:
-        position = [
-          -15,
-          -16,
-          -5,
-          -6,
-        ];
+        position = [-15, -16, -5, -6];
+        break;
       case Tetromino.S:
-        position = [
-          -15,
-          -14,
-          -6,
-          -5,
-        ];
+        position = [-15, -14, -6, -5];
+        break;
       case Tetromino.Z:
-        position = [
-          -16,
-          -17,
-          -6,
-          -5,
-        ];
+        position = [-17, -16, -6, -5];
+        break;
       case Tetromino.T:
-        position = [
-          -26,
-          -16,
-          -6,
-          -15,
-        ];
+        position = [-26, -16, -6, -15];
         break;
       default:
     }
